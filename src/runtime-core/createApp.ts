f6d9f4c9fba8ca26570d4d419d7aa64ebaa12a1a
white-letter <121116111@qq.com>
2021-09-29 00:08:13 +0800

    wip:createApp

--- src/runtime-core/createApp.ts
+++ src/runtime-core/createApp.ts
@@ -1,9 +1,11 @@
+import { render } from "./renderer";
 import { createVNode } from "./vnode";
 
 export function createApp(rootComponent) {
   return {
     mound(rootContainer) {
       const vnode = createVNode(rootComponent);
+      render(vnode, rootComponent);
     },
   };
 }
